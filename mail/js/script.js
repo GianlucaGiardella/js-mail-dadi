7d5cccfc41f35799b3d61cb0a9af4ccce6ec9c28
GianluGiarde <gianlugiarde@gmail.com>
2023-04-18 01:57:37 +0200

    add: mail control

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,22 +1,31 @@
-const mail = prompt("inserisci la tua mail");
-const mailList = [
-    "aldo@gmail.com",
-    "giovanni@gmail.com",
-    "giacomo@gmail.com",
-    "topolino@gmail.com",
-    "pippo@gmail.com",
-    "pluto@gmail.com",
-    "paperino@gmail.com"
-];
-let flag = false;
+const newMail = document.createElement("div");
+document.querySelector(".form").appendChild(newMail);
 
-console.log(mail)
+function control() {
+    const mail = document.querySelector("#mail_input").value;
+    const mailList = [
+        "aldo@gmail.com",
+        "giovanni@gmail.com",
+        "giacomo@gmail.com",
+        "topolino@gmail.com",
+        "pippo@gmail.com",
+        "pluto@gmail.com",
+        "paperino@gmail.com"
+    ];
 
-for (let i = 0; i < mailList.length; i++) {
-    if (mail === mailList[i]) {
-        console.log("Accesso effettuato")
+    if (mail != "") {
+        let flag = false;
+        for (let i = 0; i < mailList.length; i++) {
+            if (mail === mailList[i]) {
+                flag = true;
+            }
+        }
+        if (flag) {
+            newMail.className = "result green";
+            newMail.innerHTML = "Accesso effettuato";
+        } else {
+            newMail.className = "result red";
+            newMail.innerHTML = "Accesso negato";
+        }
     }
 }
-
-
-
